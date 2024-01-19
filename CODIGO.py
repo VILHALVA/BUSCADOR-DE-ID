645703cc10dec2527fdfe3b3d278b03f2adef989
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2024-01-19 14:56:49 -0500

    Update CODIGO.py

--- CODIGO.py
+++ CODIGO.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Token do bot
-token = "TOKEN_DO_BOT"
+token = "TOKEN_AQUI"
 
 # Inicializa o bot
 bot = telebot.TeleBot(token)
@@ -9,10 +9,8 @@ bot = telebot.TeleBot(token)
 # Função para lidar com a mensagem /start
 @bot.message_handler(commands=["start"])
 def start(message):
-    # Envia uma mensagem de boas-vindas
-    bot.send_message(message.chat.id, "Olá! Este bot exibirá o id do chat de uma mensagem encaminhada.")
-    # Envia instruções de uso
-    bot.send_message(message.chat.id, "Para usar o bot, envie uma mensagem encaminhada de algum grupo ou canal.")
+    # Envia uma mensagem de boas-vindas e instruções de uso:
+    bot.send_message(message.chat.id, "Olá! Este bot exibirá o id do chat de uma mensagem encaminhada.\n Para usar o bot, envie uma mensagem encaminhada (Com citação) de algum grupo ou canal.")
 
 # Função para lidar com mensagens encaminhadas
 @bot.message_handler(content_types=["text", "photo", "video", "audio", "document", "sticker", "voice"])
@@ -20,7 +18,7 @@ def mensagem_encerrada(message):
     # Obtém o id do chat da mensagem original
     id_chat = message.chat.id
     # Envia a mensagem com o id do chat
-    bot.send_message(id_chat, "ID do chat: -{}".format(message.chat.id))
+    bot.reply_to(message, "<b>ID DO CHAT:\n</b><code>-{}</code>".format(id_chat), parse_mode="html")
 
 # Inicia o bot
 bot.polling()
