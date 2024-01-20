7bb049994b99fe21e1bb610874390a4d812ff95f
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2024-01-19 23:10:16 -0500

    Update CODIGO.py

--- CODIGO.py
+++ CODIGO.py
@@ -10,15 +10,19 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def start(message):
     # Envia uma mensagem de boas-vindas e instruções de uso:
-    bot.send_message(message.chat.id, "Olá! Este bot exibirá o id do chat de uma mensagem encaminhada.\n Para usar o bot, envie uma mensagem encaminhada (Com citação) de algum grupo ou canal.")
+    bot.send_message(message.chat.id, "OLÁ! ESTE BOT EXIBIRÁ O ID DO CHAT DE UMA MENSAGEM ENCAMINHADA.\n PARA USAR O BOT, ENVIE UMA MENSAGEM ENCAMINHADA (COM CITAÇÃO) DE ALGUM GRUPO OU CANAL.")
 
 # Função para lidar com mensagens encaminhadas
 @bot.message_handler(content_types=["text", "photo", "video", "audio", "document", "sticker", "voice"])
 def mensagem_encerrada(message):
-    # Obtém o id do chat da mensagem original
-    id_chat = message.chat.id
-    # Envia a mensagem com o id do chat
-    bot.reply_to(message, "<b>ID DO CHAT:\n</b><code>-{}</code>".format(id_chat), parse_mode="html")
+    # Verifica se a mensagem é encaminhada
+    if message.forward_from_chat:
+        # Obtém o id do chat da mensagem original
+        id_chat = message.forward_from_chat.id
+        # Envia a mensagem com o id do chat
+        bot.reply_to(message, "<b>ID DO CHAT:\n</b><code>{}</code>".format(id_chat), parse_mode="html")
+    else:
+        bot.reply_to(message, "🤬ERRO: NÃO CONSEGUIR ENCONTRAR O ID. PODE ER POR UM DESSES DOIS MOTIVOS:\n1️⃣ESTA MENSAGEM NÃO FOI ENCAMINHADA COM CITAÇÃO.\n2️⃣VOCÊ ESTÁ TENTANDO ENCAMINHAR MENSAGEM PRIVADA DE USUÁRIO.")
 
 # Inicia o bot
 bot.polling()
